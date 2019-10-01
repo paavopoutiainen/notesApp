dd89fd789a79293d3d8850e28bf80b2c8f1e5854
Paavo Poutiainen <paavopoutiainen90@gmail.com>
2019-10-01 15:03:49 +0300

    added the production build of front

---
+++ build/precache-manifest.d666d1f2f7ff161a6d1b2026f00431c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4397f431394e814fe81560520ca65a84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d16f31edb08b9ce384fc",
+    "url": "/static/css/main.a290f3aa.chunk.css"
+  },
+  {
+    "revision": "1a7aa226f30e14078d8f",
+    "url": "/static/js/2.f378e79f.chunk.js"
+  },
+  {
+    "revision": "d16f31edb08b9ce384fc",
+    "url": "/static/js/main.8a6440ec.chunk.js"
+  },
+  {
+    "revision": "537d98cd4329b10fdd4d",
+    "url": "/static/js/runtime~main.c0230475.js"
+  }
+]);
